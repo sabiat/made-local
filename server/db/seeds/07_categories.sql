e4ce6a18c7656777eb649da19b36a28b40be1927
vagrant <galit.gerasimov3@gmail.com>
2021-03-13 02:26:35 +0000

    Added categories schema and seed

---
+++ server/db/seeds/07_categories.sql
@@ -0,0 +1,12 @@
+INSERT INTO categories(name, shop_id)
+  VALUES 
+  ('Food and Catering'),
+  ('Jewellery and Accessories'),
+  ('Tattoo Artists'),
+   ('Clothing'),
+   ('Events and Planning'),
+   ('Home & Living'),
+   ('Health and Wellness);
+
+
+   
