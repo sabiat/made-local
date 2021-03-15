b414fdeaa068cea6a1233c70d43601a07ee47693
vagrant <galit.gerasimov3@gmail.com>
2021-03-15 14:58:06 +0000

    Added home shop cards

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,33 +1,43 @@
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
-import ShopBadge from "./ShopBadge";
 import SearchBar from "./SearchBar";
+import Card from "./Card";
 
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Home() {
-  const [state, setState] = useState();
+  const [shop, setShop] = useState([]);
 
   useEffect(() => {
     axios
       .get("/api/shops")
       .then((res) => {
-        setState(res.data);
+        setShop(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
-  console.log("this is the state: ", state);
+  console.log("this is the state: ", shop);
   return (
     <div>
       <SearchBar />
-      <Grid container direction="row" justify="center" alignItems="center">
-        <ShopBadge
-          shopName={state}
-          // shopName={state.name}
+      {shop.map((shop) => (
+        <Card
+          name={shop.name}
+          description={shop.description}
+          delivery={shop.delivery}
+          pickup={shop.pickup}
+          shipping={shop.shipping}
+          photo={shop.photo}
         />
-      </Grid>
+      ))}
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      ></Grid>
     </div>
   );
 }
