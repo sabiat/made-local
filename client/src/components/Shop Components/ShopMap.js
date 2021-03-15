6ef7418db335d84ece04f60ff3914a4ccf735ec7
Sabia Tahir <sabiatahir@hotmail.com>
2021-03-15 00:11:21 -0400

    Render map on shop page

--- client/src/components/Shop Components/ShopMap.js
+++ client/src/components/Shop Components/ShopMap.js
@@ -1,3 +1,24 @@
+import React, { Component } from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import "../../App.css";
+
+
 export default function ShopMap() {
-  return <h2>ShopMap</h2>
+
+  return (
+  <div>
+    <h2>ShopMap</h2>
+    <MapContainer
+      className='map-container'
+      center={[45.529753, -73.586187]} 
+      zoom={17}
+    >
+      <Marker position={[45.529753, -73.586187]}></Marker>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
+  </div>
+  )
 }
