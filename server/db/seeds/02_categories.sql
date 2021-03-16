ab2d536cecad70a34c29f8e600161afe5cc37738
Sabia Tahir <sabiatahir@hotmail.com>
2021-03-16 01:49:56 -0400

    fix categories

--- server/db/seeds/02_categories.sql
+++ server/db/seeds/02_categories.sql
@@ -2,7 +2,6 @@ INSERT INTO categories(name)
   VALUES 
   ('Food & Catering'),
   ('Jewellery & Accessories'),
-  ('Tattoo Artists'),
   ('Clothing'),
   ('Events & Planning'),
   ('Home & Living'),
