3063f77275c8e8fa052c8e72d3fbb88a614fce1f
Sabia Tahir <sabiatahir@hotmail.com>
2021-03-17 00:30:45 -0400

    Render login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,3 +1,83 @@
+import { React, useState } from "react";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(3),
+  },
+}));
+
 export default function Login() {
-  return <h2>login</h2>;
+  const classes = useStyles();
+
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const sendUserInfo = () => {
+    if (userInfo.length && userInfo.password.length) {
+    }
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+  return (
+    <Container className={classes.container} maxWidth="xs">
+      <form>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Email"
+                  name="email"
+                  value={userInfo.email}
+                  onChange={handleChange}
+                  size="small"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Password"
+                  name="password"
+                  value={userInfo.password}
+                  onChange={handleChange}
+                  size="small"
+                  type="password"
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              color="secondary"
+              fullWidth
+              type="submit"
+              variant="contained"
+              onClick={handleSubmit}
+            >
+              Log in
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
 }
--- client/src/components/Root.js
+++ client/src/components/Root.js
@@ -1,18 +1,23 @@
 // import Button from "./components/Button";
 import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router-dom";
 
 export default function Root() {
+  const history = useHistory();
   return (
     <div>
       <div>
-        <img src="https://images.pexels.com/photos/6097827/pexels-photo-6097827.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="home" />
+        <img
+          src="https://images.pexels.com/photos/6097827/pexels-photo-6097827.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          alt="home"
+        />
       </div>
       <h2>Root</h2>
       <Button
         variant="contained"
         color="secondary"
         onClick={() => {
-          console.log("CLICKED");
+          history.push("/login");
         }}
       >
         Login
@@ -21,7 +26,7 @@ export default function Root() {
         variant="contained"
         color="secondary"
         onClick={() => {
-          console.log("CLICKED");
+          history.push("/register");
         }}
       >
         Register
