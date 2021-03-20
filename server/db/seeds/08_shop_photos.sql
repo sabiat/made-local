c8d0a46cc797298b0b4deeb88dd13b491e6adbb5
Sabia Tahir <sabiatahir@hotmail.com>
2021-03-20 04:54:54 +0000

    Add more fake data

--- server/db/seeds/08_shop_photos.sql
+++ server/db/seeds/08_shop_photos.sql
@@ -3,5 +3,27 @@ INSERT INTO shop_photos(shop_id, photo_urls)
   (1, 'https://images.pexels.com/photos/1070891/pexels-photo-1070891.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
   (1, 'https://images.pexels.com/photos/4099124/pexels-photo-4099124.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'), 
   (1, 'https://images.pexels.com/photos/2034484/pexels-photo-2034484.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (1, 'https://images.pexels.com/photos/5633980/pexels-photo-5633980.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
   (2, 'https://images.pexels.com/photos/4792074/pexels-photo-4792074.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'),
-  (2, 'https://images.pexels.com/photos/1030946/pexels-photo-1030946.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+  (2, 'https://images.pexels.com/photos/1030946/pexels-photo-1030946.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (2, 'https://images.pexels.com/photos/5464589/pexels-photo-5464589.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
+  (3, 'https://images.pexels.com/photos/4210864/pexels-photo-4210864.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (3, 'https://images.pexels.com/photos/4614217/pexels-photo-4614217.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (3, 'https://images.pexels.com/photos/1943846/pexels-photo-1943846.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (4, 'https://images.pexels.com/photos/5946038/pexels-photo-5946038.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (4, 'https://images.pexels.com/photos/264892/pexels-photo-264892.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (4, 'https://images.pexels.com/photos/1579926/pexels-photo-1579926.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (5, 'https://images.pexels.com/photos/1612847/pexels-photo-1612847.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (5, 'https://images.pexels.com/photos/5749777/pexels-photo-5749777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (5, 'https://images.pexels.com/photos/5732499/pexels-photo-5732499.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'),
+  (6, 'https://images.pexels.com/photos/3323682/pexels-photo-3323682.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (6, 'https://images.pexels.com/photos/4457151/pexels-photo-4457151.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (6, 'https://images.pexels.com/photos/6210887/pexels-photo-6210887.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (7, 'https://images.pexels.com/photos/5645173/pexels-photo-5645173.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (7, 'https://images.pexels.com/photos/2456435/pexels-photo-2456435.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (7, 'https://images.pexels.com/photos/4147872/pexels-photo-4147872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (7, 'https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'),
+  (7, 'https://images.pexels.com/photos/4099235/pexels-photo-4099235.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
+
+
+
