48f4e7a6108b56e996339aca3ac113d9e953b112
Sabia Tahir <sabiatahir@hotmail.com>
2021-03-22 02:28:16 +0000

    remove import in photos and change backgroudn colour

--- client/src/components/Shop Components/ShopPhotos.js
+++ client/src/components/Shop Components/ShopPhotos.js
@@ -1,7 +1,5 @@
 import { React, useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Img from "react-cool-img";
 import Grid from "@material-ui/core/Grid";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import axios from "axios";
