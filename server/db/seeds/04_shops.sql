80c49c2ba55c8cd0e92b13d5e4f9e9807b75a3c9
Sabia Tahir <sabiatahir@hotmail.com>
2021-03-22 02:16:02 +0000

    Add styling to shop profile

--- server/db/seeds/04_shops.sql
+++ server/db/seeds/04_shops.sql
@@ -1,11 +1,11 @@
 INSERT INTO shops (name, description, street_address, postal_code, city, latitude, longitude, phone_number, social, photo, user_id, delivery, pickup, shipping, category_id)
 VALUES
 
-('The Original Cupcakes', 'Delicious Cupcakes Baked Fresh Daily', '191 Rue Saint-Zotique Est','H2S 1K9', 'Montreal', 45.53341, -73.610475,'514-555-5555', 'https://www.instagram.com/theoriginalcupcakes/?hl=en', 'https://media1.popsugar-assets.com/files/thumbor/7YxGDN4ZCczmAFCz34ZgG8meZ74/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/03/05/727/n/45101125/4a9b42c15c7ea36448b104.24111438_/i/Wilton-Cakes-Instagram.jpg', 1, true, true, true, 1),
+('The Original Cupcakes', 'Delicious Cupcakes Baked Fresh Daily', '191 Rue Saint-Zotique Est','H2S 1K9', 'Montreal', 45.53341, -73.610475,'514-555-5555', '@theogcupcakes', 'https://media1.popsugar-assets.com/files/thumbor/7YxGDN4ZCczmAFCz34ZgG8meZ74/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/03/05/727/n/45101125/4a9b42c15c7ea36448b104.24111438_/i/Wilton-Cakes-Instagram.jpg', 1, true, true, true, 1),
 
 ('String Things', 'Handmade by me in Montreal, QC, Canada', '5065 Avenue de Mentana', 'H4C2H4', 'Montreal', 43.641840, -79.386440, '514-555-5555', '@stringy__things', 'https://images.pexels.com/photos/1030946/pexels-photo-1030946.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 2, true, true, true, 4),
 
-('Shop the Constant Closet', 'Special finds from my closet', '4065 Avenue de Mentana', 'H4C2H4', 'Montreal', 45.529753, -73.586187, '514-555-5555', '@shoptheconstantcloset', 'https://images.pexels.com/photos/4301252/pexels-photo-4301252.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 3, false, true, true, 4),
+('Constant Closet', 'Special finds from my closet', '4065 Avenue de Mentana', 'H4C2H4', 'Montreal', 45.529753, -73.586187, '514-555-5555', '@shoptheconstantcloset', 'https://images.pexels.com/photos/4301252/pexels-photo-4301252.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 3, false, true, true, 4),
 
 ('Parc sans gluten', 'Contact us today to order!', '4050 Avenue du Parc-La Fontaine', 'H2L 3M8', 'Montreal', 45.5213221,-73.588299, '514-555-5555', '@glutenfree.catering', 'https://images.pexels.com/photos/2067396/pexels-photo-2067396.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260', 4, false, true, false, 1),
 
